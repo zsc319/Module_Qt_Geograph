4f8380170273c0a90a7c594b2f2bdd78fe586a55
LukeLinux <wulinshuxue@qq.com>
2017-01-17 17:37:39 +0800

    change include file name

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include <QCoreApplication>
 #include <QDebug>
 #include "Module_Qt_GeoShapes/MyQtGeoPolygon.h"
-#include "Module_Qt_GeoShapes/myqtgeorectangle.h"
+#include "Module_Qt_GeoShapes/MyQtGeoRectangle.h"
 #include <QGeoCoordinate>
 int main(int argc, char *argv[])
 {
